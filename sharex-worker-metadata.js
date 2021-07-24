46251872a37649ff74c365961288303224d249bf
CubityFirst <cubity@cubityfir.st>
2021-07-24 21:24:17 +0100

    Update sharex-worker-metadata.js

--- sharex-worker-metadata.js
+++ sharex-worker-metadata.js
@@ -1,34 +1,41 @@
 async function handleRequest(request) {
-    const userAgent = request.headers.get("User-Agent");
-    if (userAgent && userAgent.includes("Discord")) {
-      const url = new URL(request.url);
-      const title = "Title";
-      const description = url.pathname.substr(url.pathname.lastIndexOf("/") + 1);
-      return new Response(`<html>
-  <head>
-    <title>${title}</title>
-    <meta charset="utf-8">
-    <meta name="robots" content="noindex">
-    <meta name="theme-color" content="#ffffff">
-    <meta name="description" content="${description}">
-    <meta property="og:description" content="${description}">
-    <meta property="og:title" content="${title}">
-    <meta property="og:image" content="${url.toString()}">
-    <meta property="twitter:card" content="summary_large_image">
-    <meta property="og:url" content="${url.toString()}">
-  </head>
-  <body>
-    <img src="${url.toString()}">
-  </body>
-  </html>`, {
-        headers: {
-          "content-type": "text/html"
-        }
-      });
+  const userAgent = request.headers.get("user-agent");
+  if (userAgent && userAgent.includes("Discord")) {
+    const response = await fetch(request);
+    if (response.status == 200) {
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.startsWith("image/")) {
+        const url = new URL(request.url);
+        const title = "ShareX";
+        const description = url.pathname.substr(url.pathname.lastIndexOf("/") + 1);
+        const color = "#00aff4";
+        return new Response(`<html>
+<head>
+  <title>${title}</title>
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex">
+  <meta name="theme-color" content="${color}">
+  <meta name="description" content="${description}">
+  <meta property="og:title" content="${title}">
+  <meta property="og:description" content="${description}">
+  <meta property="og:image" content="${url.toString()}">
+  <meta property="twitter:card" content="summary_large_image">
+</head>
+<body>
+  <img src="${url.toString()}">
+</body>
+</html>`, {
+          headers: {
+            "content-type": "text/html"
+          }
+        });
+      }
     }
-    return fetch(request);
+    return response;
   }
-  
-  addEventListener("fetch", event => {
-    event.respondWith(handleRequest(event.request));
-  });
+  return fetch(request);
+}
+
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
